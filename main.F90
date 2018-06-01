79080fe63d93184d46472449ad0e57b58c8f817a
Grucha8 <t.szamot1997@gmail.com>
2018-06-01 17:08:01 +0200

    Init commit

---
+++ main.F90
@@ -0,0 +1,14 @@
+module mult
+contains
+    subroutine mm(first, second, multiply, status)
+        implicit none
+        real (kind = 8), intent(in) :: first(:,:) ! pierwsza macierz
+        real (kind = 8), intent(in) :: second(: ,:) ! druga macierz
+        real (kind = 8), intent(out) :: multiply(:,:) ! macierz wynikowa
+        integer (kind = 4), intent(out) :: status ! kod błędu, 0 gdy OK
+    
+        
+
+    end subroutine
+
+end module
